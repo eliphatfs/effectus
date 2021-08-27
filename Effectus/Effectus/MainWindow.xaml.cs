bccbfb6d3a8867bd3f6ea366901cf86bf8e2b3a8
eliphatfs <flandre@scarletx.cn>
2021-08-27 16:05:46 +0800

    Tried MediaElement in WPF Not usable. Need to reinvent.

--- Effectus/Effectus/MainWindow.xaml.cs
+++ Effectus/Effectus/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -25,9 +26,60 @@ namespace Effectus
             InitializeComponent();
         }
 
+        internal readonly Storyboard storyboard = new();
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+            storyboard.SlipBehavior = SlipBehavior.Slip;
+            MediaTimeline video1 = new(new Uri("Metal_Wind_Chimes_at_Sunset.mp4", UriKind.Relative));
+            video1.BeginTime = TimeSpan.FromSeconds(3);
+            MediaTimeline video2 = new(new Uri("Metal_Wind_Chimes_at_Sunset.mp4", UriKind.Relative));
+            video2.BeginTime = TimeSpan.FromSeconds(3);
+            video1.CurrentTimeInvalidated += Storyboard_CurrentTimeInvalidated;
+            video2.CurrentTimeInvalidated += Storyboard_CurrentTimeInvalidated;
+            Storyboard.SetTarget(video1, testVideo1);
+            Storyboard.SetTarget(video2, testVideo2);
+            storyboard.Children.Add(video1);
+            storyboard.Children.Add(video2);
+            storyboard.Begin(this, true);
+        }
+
+        private void Storyboard_CurrentTimeInvalidated(object sender, EventArgs e)
+        {
+            videoTimesLabel.Content = $"{testVideo1.Position.TotalMilliseconds:0.0}\n{testVideo2.Position.TotalMilliseconds:0.0}";
+        }
+
         private void MenuItemExit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void RandomJump_Click(object sender, RoutedEventArgs e)
+        {
+            storyboard.SeekAlignedToLastTick(
+                this, TimeSpan.FromSeconds(new Random().NextDouble() * 15),
+                TimeSeekOrigin.BeginTime
+            );
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            storyboard.Resume(this);
+        }
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            storyboard.Pause(this);
+            Storyboard_CurrentTimeInvalidated(sender, e);
+        }
+
+        private void Step_Click(object sender, RoutedEventArgs e)
+        {
+            storyboard.SeekAlignedToLastTick(
+                this, storyboard.GetCurrentTime(this).Value + TimeSpan.FromMilliseconds(10),
+                TimeSeekOrigin.BeginTime
+            );
+            Storyboard_CurrentTimeInvalidated(sender, e);
         }
     }
 }
