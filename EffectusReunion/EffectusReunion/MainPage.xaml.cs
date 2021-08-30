3bd5bd2e8803da4c339f614071747a6954c519a5
eliphatfs <flandre@scarletx.cn>
2021-08-30 18:29:08 +0800

    It syncs!

--- EffectusReunion/EffectusReunion/MainPage.xaml.cs
+++ EffectusReunion/EffectusReunion/MainPage.xaml.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media;
+using Windows.Media.Core;
+using Windows.Media.Playback;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,29 +25,52 @@ namespace EffectusReunion
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        MediaTimelineController controller;
         public MainPage()
         {
             this.InitializeComponent();
+            CompositionTarget.Rendering += CompositionTarget_Rendered;
+        }
+
+        private void CompositionTarget_Rendered(object sender, object e)
+        {
+            timeLabels.Text = $"{video1.MediaPlayer.PlaybackSession.Position.TotalMilliseconds}\n{video2.MediaPlayer.PlaybackSession.Position.TotalMilliseconds}";
         }
 
         private void Play_Click(object sender, RoutedEventArgs e)
         {
-
+            if (controller == null)
+            {
+                controller = new MediaTimelineController();
+                video1.SetMediaPlayer(new Windows.Media.Playback.MediaPlayer());
+                video2.SetMediaPlayer(new Windows.Media.Playback.MediaPlayer());
+                video1.MediaPlayer.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///Metal_Wind_Chimes_at_Sunset.mp4"));
+                video2.MediaPlayer.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///Metal_Wind_Chimes_at_Sunset.mp4"));
+                video1.MediaPlayer.CommandManager.IsEnabled = false;
+                video2.MediaPlayer.CommandManager.IsEnabled = false;
+                video1.MediaPlayer.TimelineController = controller;
+                video2.MediaPlayer.TimelineController = controller;
+                controller.Start();
+            }
+            else
+                controller.Resume();
         }
 
         private void RandomJump_Click(object sender, RoutedEventArgs e)
         {
-
+            controller.Position = TimeSpan.FromSeconds(new Random().NextDouble() * 15);
         }
 
         private void Pause_Click(object sender, RoutedEventArgs e)
         {
-
+            controller.Pause();
         }
 
         private void Step_Click(object sender, RoutedEventArgs e)
         {
-
+            var old = controller.Position;
+            controller.Position = TimeSpan.Zero;
+            controller.Position = old + TimeSpan.FromMilliseconds(10);
         }
     }
 }
