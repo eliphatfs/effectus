0dff4447fef6da5b4f11ddf22ef8f6f7d9fa9e02
eliphatfs <flandre@scarletx.cn>
2021-08-31 22:17:19 +0800

    Virtual Layer Starting

---
+++ EffectusReunion/EffectusReunion/VirtualMediaObjectModel/VirtualMediaNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.Audio;
+using Windows.UI.Xaml;
+using EffectusReunion.VirtualTransport;
+
+namespace EffectusReunion.VirtualMediaObjectModel
+{
+    /// <summary>
+    /// Base class for all descriptors and logical nodes.
+    /// Produces audio and video entity nodes.
+    /// </summary>
+    public abstract class VirtualMediaNode
+    {
+        public abstract FrameworkElement CreateVisualNode();
+        public abstract IAudioNode CreateAudioNode(AudioGraph graph);
+        public virtual void Update(VirtualTransportControl transport)
+        {
+        }
+    }
+}
---
+++ EffectusReunion/EffectusReunion/VirtualTransport/VirtualTransportControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media;
+
+namespace EffectusReunion.VirtualTransport
+{
+    /// <summary>
+    /// Global states for transport control.
+    /// </summary>
+    public class VirtualTransportControl
+    {
+        public TimeSpan Time { get; set; } = TimeSpan.Zero;
+        public MediaTimelineController MediaTimelineController
+        {
+            get; private set;
+        } = new MediaTimelineController();
+    }
+}
