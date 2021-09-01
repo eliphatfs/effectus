db7d5b36331022b659da0de19456a12158066dd3
eliphatfs <flandre@scarletx.cn>
2021-09-01 16:58:55 +0800

    VMOM: Text Node

---
+++ EffectusReunion/EffectusReunion/VirtualMediaObjectModel/VirtualMediaTextNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.Media.Audio;
+
+namespace EffectusReunion.VirtualMediaObjectModel
+{
+    public class VirtualMediaTextNode : VirtualMediaNode<TextBlock, AudioSubmixNode>
+    {
+        protected override AudioSubmixNode CreateAudioNode(AudioGraph graph)
+        {
+            return graph.CreateSubmixNode();
+        }
+
+        protected override TextBlock CreateVisualNode()
+        {
+            return new TextBlock();
+        }
+    }
+}
