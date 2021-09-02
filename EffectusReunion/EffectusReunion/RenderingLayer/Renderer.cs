58c0a01cbeeee92819358b58fb312772557d1a31
eliphatfs <flandre@scarletx.cn>
2021-09-02 11:48:55 +0800

    Temporary Rendering Layer

---
+++ EffectusReunion/EffectusReunion/RenderingLayer/Renderer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using Microsoft.Graphics.Canvas;
+
+namespace EffectusReunion.RenderingLayer
+{
+    public class Renderer
+    {
+        public Renderer Parent;
+        public Matrix4x4 LocalTransform;
+        public Matrix4x4 InheritedTransform => Parent?.GlobalTransform ?? Matrix4x4.Identity;
+        public Matrix4x4 GlobalTransform => InheritedTransform * LocalTransform;
+        public virtual void Render(CanvasDrawingSession session)
+        {
+        }
+    }
+}
