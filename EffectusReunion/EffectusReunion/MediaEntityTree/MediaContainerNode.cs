0522f1bf5ba8fa4671d28567747f2d90f8bff4b1
eliphatfs <flandre@scarletx.cn>
2021-09-02 22:41:14 +0800

    MET: Implement MediaContainerNode

--- EffectusReunion/EffectusReunion/MediaEntityTree/MediaContainerNode.cs
+++ EffectusReunion/EffectusReunion/MediaEntityTree/MediaContainerNode.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 using Windows.Media.Audio;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -14,11 +15,17 @@ namespace EffectusReunion.MediaEntityTree
     public class MediaContainerNode : IMediaEntityNode
     {
         private readonly List<IMediaEntityNode> _children = new();
-        public IReadOnlyList<IMediaEntityNode> Children => _children;
-
+        public IList<IMediaEntityNode> Children => _children;
+        public Matrix3x2 Transform = Matrix3x2.Identity;
         public void Render(TransportControl transport, CanvasDrawingSession canvas, AudioFrameInputNode audio)
         {
-            throw new NotImplementedException();
+            var oldTransform = canvas.Transform;
+            canvas.Transform *= Transform;
+            foreach (IMediaEntityNode child in Children)
+            {
+                child.Render(transport, canvas, audio);
+            }
+            canvas.Transform = oldTransform;
         }
     }
 }
