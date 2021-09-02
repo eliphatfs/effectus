dc2ab51e60d849533c3aea31138cf31a0cc9ed8a
eliphatfs <flandre@scarletx.cn>
2021-09-02 22:40:53 +0800

    Media Entity Exception

---
+++ EffectusReunion/EffectusReunion/MediaEntityTree/MediaEntityException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EffectusReunion.MediaEntityTree
+{
+
+    [Serializable]
+    public class MediaEntityException : Exception
+    {
+        public MediaEntityException()
+        {
+        }
+        public MediaEntityException(string message) : base(message) { }
+        public MediaEntityException(string message, Exception inner) : base(message, inner) { }
+        protected MediaEntityException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
