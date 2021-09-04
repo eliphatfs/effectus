cc47acbf7908bf4aa1d87011cfeabd0966756388
eliphatfs <flandre@scarletx.cn>
2021-09-04 22:10:16 +0800

    MET: Media Text Node

---
+++ EffectusReunion/EffectusReunion/MediaEntityTree/MediaTextNode.cs
@@ -0,0 +1,27 @@
+﻿using EffectusReunion.MediaTransportSystem;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Media.Audio;
+using Windows.UI;
+
+namespace EffectusReunion.MediaEntityTree
+{
+    public class MediaTextNode : IMediaEntityNode
+    {
+        public string Text = "";
+        public Color Foreground = Colors.WhiteSmoke;
+        public CanvasTextFormat TextFormat = new();
+        public Vector2 LocalPosition;
+        public void Render(TransportControl transport, CanvasDrawingSession canvas, AudioFrameInputNode audio)
+        {
+            canvas.DrawText(Text, LocalPosition, Foreground, TextFormat);
+        }
+    }
+}
