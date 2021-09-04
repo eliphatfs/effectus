c312034d3b217e55de5a0ccc09fb463529a53cfd
eliphatfs <flandre@scarletx.cn>
2021-09-04 21:22:57 +0800

    MET: Untested Finished Media Loader Node

--- EffectusReunion/EffectusReunion/MediaEntityTree/MediaLoaderNode.cs
+++ EffectusReunion/EffectusReunion/MediaEntityTree/MediaLoaderNode.cs
@@ -53,29 +53,48 @@ namespace EffectusReunion.MediaEntityTree
             node.videoLoader.IsVideoFrameServerEnabled = true;
             node.audioSubgraph.QuantumStarted += node.AudioSubgraph_QuantumStarted;
             node.videoLoader.VideoFrameAvailable += node.VideoLoader_VideoFrameAvailable;
-            node.videoLoader.PlaybackSession.PositionChanged += PlaybackSession_PositionChanged;
+            node.videoLoader.PlaybackSession.PositionChanged += node.PlaybackSession_PositionChanged;
             return node;
         }
 
-        private static void PlaybackSession_PositionChanged(MediaPlaybackSession sender, object args)
-        {
-            throw new NotImplementedException();
-        }
+        protected TimeSpan videoTimeline = TimeSpan.MaxValue;
+        protected TimeSpan alignedVideoTimeline = TimeSpan.MaxValue;
+        protected TaskCompletionSource<bool> videoSeekDispatcher = new();
+        protected TaskCompletionSource<bool> videoDispatcher = new();
+        protected CanvasRenderTarget videoSink = null;
 
-        protected TaskCompletionSource<AudioFrame> audioDispatcher = new();
+        private void PlaybackSession_PositionChanged(MediaPlaybackSession sender, object args)
+        {
+            alignedVideoTimeline = sender.Position;
+            videoSeekDispatcher.TrySetResult(true);
+        }
         private void VideoLoader_VideoFrameAvailable(MediaPlayer sender, object args)
         {
-            throw new NotImplementedException();
+            if (videoSink != null)
+            {
+                sender.CopyFrameToVideoSurface(
+                    videoSink,
+                    new(0, 0, videoSink.SizeInPixels.Width, videoSink.SizeInPixels.Height)
+                );
+                videoDispatcher.TrySetResult(true);
+            }
         }
 
+        protected TaskCompletionSource<bool> audioDispatcher = new();
+        protected TimeSpan audioTimeline = TimeSpan.MaxValue;
+        protected TimeSpan requestedTimeline;
+        protected AudioFrameInputNode audioSink;
         private void AudioSubgraph_QuantumStarted(AudioGraph sender, object args)
         {
             if (!audioDispatcher.Task.IsCompleted)
             {
                 var frame = audioReader.GetFrame();
-                if (frame.Duration.HasValue && frame.Duration.Value > TimeSpan.Zero)
+                audioSink.AddFrame(frame);
+                audioTimeline += frame.Duration.Value;
+                if (audioTimeline >= requestedTimeline + TimeSpan.FromMilliseconds(120))
                 {
-                    audioDispatcher.SetResult(frame);
+                    audioDispatcher.SetResult(true);
+                    audioSubgraph.Stop();
                 }
             }
 
@@ -83,7 +102,54 @@ namespace EffectusReunion.MediaEntityTree
 
         public void Render(TransportControl transport, CanvasDrawingSession canvas, AudioFrameInputNode audio)
         {
-            // canvas.DrawImage(null, LocalRenderRect);
+            requestedTimeline = transport.Time;
+            audioSink = audio;
+            if (videoSink == null
+                || videoSink.SizeInPixels.Width != (int)(0.5 + LocalRenderRect.Width)
+                || videoSink.SizeInPixels.Height != (int)(0.5 + LocalRenderRect.Height)
+            )
+            {
+                if (videoSink != null)
+                    videoSink.Dispose();
+                videoSink = new(CanvasDevice.GetSharedDevice(), (float)LocalRenderRect.Width, (float)LocalRenderRect.Height, 96);
+            }
+            if (!transport.IsPlaying)
+            {
+                audioTimeline = TimeSpan.MaxValue;
+                if (videoTimeline != transport.Time)
+                {
+                    videoLoader.PlaybackSession.Position = transport.Time;
+                    videoTimeline = transport.Time;
+                }
+                canvas.DrawImage(videoSink, LocalRenderRect);
+            }
+            else
+            {
+                if (audioTimeline == TimeSpan.MaxValue)
+                {
+                    audioLoader.Seek(audioTimeline = requestedTimeline);
+                    audioReader.GetFrame();
+                }
+                audioDispatcher = new();
+                audioSubgraph.Start();
+                if (alignedVideoTimeline >= transport.Time || alignedVideoTimeline < transport.Time - TimeSpan.FromMilliseconds(100))
+                {
+                    videoSeekDispatcher = new();
+                    videoLoader.PlaybackSession.Position = transport.Time;
+                    videoSeekDispatcher.Task.Wait();
+                    videoDispatcher = new();
+                }
+                while (alignedVideoTimeline < transport.Time)
+                {
+                    videoSeekDispatcher = new();
+                    videoLoader.StepForwardOneFrame();
+                    videoSeekDispatcher.Task.Wait();
+                    videoDispatcher = new();
+                }
+                videoDispatcher.Task.Wait();
+                canvas.DrawImage(videoSink, LocalRenderRect);
+                audioDispatcher.Task.Wait();
+            }
         }
     }
 }
