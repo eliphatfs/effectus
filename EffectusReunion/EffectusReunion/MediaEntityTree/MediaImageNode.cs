f0d8f59f9a8115794263cd67ef259a3d4328b21e
eliphatfs <flandre@scarletx.cn>
2021-09-05 09:21:46 +0800

    MET: Media Image Node

---
+++ EffectusReunion/EffectusReunion/MediaEntityTree/MediaImageNode.cs
@@ -0,0 +1,25 @@
+﻿using EffectusReunion.MediaTransportSystem;
+using Microsoft.Graphics.Canvas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Media.Audio;
+
+namespace EffectusReunion.MediaEntityTree
+{
+    public class MediaImageNode : IMediaEntityNode
+    {
+        public CanvasBitmap Image;
+        public Rect LocalRenderRect
+        {
+            get; set;
+        }
+        public void Render(TransportControl transport, CanvasDrawingSession canvas, AudioFrameInputNode audio)
+        {
+            canvas.DrawImage(Image, LocalRenderRect);
+        }
+    }
+}
